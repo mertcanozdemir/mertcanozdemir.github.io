876fb7d7f326b3aa9f766cb5570de57d9399b49c
Mertcan Özdemir <58743465+mertcanozdemir@users.noreply.github.com>
2022-06-13 21:20:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 
+      function toggleNavigation() {
+        let nav = document.getElementById("mobile-nav");
+        if (nav.classList.contains('w3-show')) {
+          nav.classList.remove('w3-show');
+        } else { 
+          nav.classList.add('w3-show');
+        }
+      }
